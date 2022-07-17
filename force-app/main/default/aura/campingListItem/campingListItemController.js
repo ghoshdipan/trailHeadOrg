bc38e37ec969cf44f7592cd78fdc96f42f1da6e9
Dipan Ghosh <dghosh133@gmail.com>
2022-07-17 11:29:54 +0530

    Handle Actions with Controllers

--- force-app/main/default/aura/campingListItem/campingListItemController.js
+++ force-app/main/default/aura/campingListItem/campingListItemController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
+    packItem: function(component, event, helper) {
+        let btnClicked = event.getSource();
 
+        component.set("v.Packed__c", true);
+        component.set("v.disabled", true);
     }
 })
